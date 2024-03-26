3cf39f2036eea6a077b103f8e36342f04245da9d
XJIE6yIIIEK <112707086+XJIE6yIlIek@users.noreply.github.com>
2024-03-26 17:50:06 +0300

    Add files via upload

--- java/pr3/Fish.java
+++ java/pr3/Fish.java
@@ -1,14 +1,14 @@
 abstract class Fish implements Animal{
 
-    public final void move(){
+    public void move(){
         System.out.println("Swimming in the water");
     }
 
-    public final void eat(){
+    public void eat(){
         System.out.println("Eating stuff");
     }
 
-    public final void sleep(){
+    public void sleep(){
         System.out.println("Sleeping under the water");
     }
 
---
+++ java/pr3/Salmon.java
@@ -0,0 +1,19 @@
+public class Salmon extends Fish {
+
+    public final void make_sound(){
+        System.out.println("...");
+    }
+
+    public final void move(){
+        System.out.println("Swimming in the water");
+    }
+
+    public final void eat(){
+        System.out.println("Eating fish food");
+    }
+
+    public final void sleep(){
+        System.out.println("Sleeping in the water");
+    }
+
+}
